7f78e94f1e8ebc840854de481477292c43009ac5
Talha <talhapatel5412@gmail.com>
2022-08-23 18:42:10 +0200

    first commit

---
+++ uber_clone/screens/HomeScreen.js
@@ -0,0 +1,50 @@
+import { Image, SafeAreaView, View } from "react-native";
+import React from "react";
+import tw from "tailwind-react-native-classnames";
+import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
+import NavFavourites from "../components/NavFavourites";
+
+const HomeScreen = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{ width: 100, height: 100, resizeMode: "contain" }}
+          source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png",
+          }}
+        />
+        <GooglePlacesAutocomplete
+          placeholder="Where From?"
+          styles={{ container: { flex: 0 }, textInput: { fontSize: 18 } }}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+            dispatch(setDestination(null));
+          }}
+          fetchDetails={true}
+          returnKeyType={"search"}
+          enablePoweredByContainer={false}
+          minLength={2}
+          query={{ key: GOOGLE_MAPS_APIKEY, language: "en" }}
+          nearbyPlacesAPI="GooglePlaceSearch"
+          debounce={400}
+        />
+        <NavOptions />
+        <NavFavourites />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
